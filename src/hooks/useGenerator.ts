55f6cdb6ee5b72b7d152de0c114ebaf81fc2acdc
dheenad017 <dheenadayalan.r.s.139@kalvium.community>
2026-05-01 19:26:02 +0530

    feat(hooks): update useGenerator hook

--- src/hooks/useGenerator.ts
+++ src/hooks/useGenerator.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { GeneratorContext } from '../contexts/GeneratorContext';
+import { GeneratorContext } from '@/contexts/GeneratorContext';
 
 export function useGenerator() {
   const context = useContext(GeneratorContext);
@@ -8,3 +8,5 @@ export function useGenerator() {
   }
   return context;
 }
+
+
