90e4a3ec04a07ac80af39df150afee2310034bf9
dheenad017 <dheenadayalan.r.s.139@kalvium.community>
2026-05-01 21:43:32 +0530

    Update backend/database/models/__init__.py

--- backend/database/models/__init__.py
+++ backend/database/models/__init__.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel
 from .user import User, UserProfile, UserBalance, UserSettings
 from .projects import Project, ProductionSession, Episode, Scene, Series, Script, ScriptVersion, Storyboard, ProjectContent
-from .assets import Template, MediaAsset, UserFavorite, SavedPrompt, PromptLibrary, Prompt
+from .assets import Template, MediaAsset, UserFavorite, SavedPrompt, PromptLibrary, Prompt, GrowthStrategy
 from .system import (
     Category, Tutorial, Notification, SEOEntry, HelpCategory, FAQ, 
     DocSection, DocArticle, ScreeningRoomEntry, CommunityPost
@@ -16,7 +16,7 @@ __all__ = [
     "User", "UserProfile", "UserBalance", "UserSettings",
     "Project", "ProductionSession", "Episode", "Scene", "Series", "Script", "ScriptVersion", "Storyboard", "ProjectContent",
     "WorldLore", "CastMember", "NarrativeBeat", "ReusableCharacter",
-    "Template", "MediaAsset", "UserFavorite", "SavedPrompt", "PromptLibrary", "Prompt",
+    "Template", "MediaAsset", "UserFavorite", "SavedPrompt", "PromptLibrary", "Prompt", "GrowthStrategy",
     "Category", "Tutorial", "Notification", "SEOEntry", "HelpCategory", "FAQ",
     "DocSection", "DocArticle", "ScreeningRoomEntry", "CommunityPost",
     "SystemLog"
